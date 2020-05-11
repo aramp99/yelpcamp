2fb8a88c3703da7b5f311add389ec895273b4b76
Ram Prashanth <aramp99@yahoo.co.in>
2020-05-11 18:20:59 +0530

    added mongo atlas

--- app.js
+++ app.js
@@ -18,7 +18,15 @@ var CampgroundRoutes        = require("./routes/campground"),
 
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useFindAndModify', false);
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true } );
+mongoose.connect("mongodb+srv://dbUser:ram-RAM-123@cluster0-n9skn.mongodb.net/test?retryWrites=true&w=majority", 
+{   useNewUrlParser: true,
+    useCreateIndex:  true } 
+).then(()=> {
+    console.log("Connected to DB!");
+}).catch(err=> { 
+    console.log("ERROR", err.message);
+});
+
 app.set("view engine", "ejs");
 app.use(flash());
 app.use(bodyparse.urlencoded({extended: true}));
